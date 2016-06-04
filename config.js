96d132440a57a6c59271092a178944d36716778f
Bushman <eljamaki01@hotmail.com>
2016-06-04 12:38:51 -0700

    Changed the global stories _id to under my control

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 config.JWT_SECRET = "<yoursecretkey>";
 config.FAROO_KEY = "<yourfarookey>";
 config.MONGODB_CONNECT_URL = "mongodb://<username>:<password>@ds015710.mlab.com:15710/newswatcherdb";
-config.GLOBAL_STORIES_ID = "56b3b297e4b0b133d46da115";
+config.GLOBAL_STORIES_ID = "MASTER_STORIES_DO_NOT_DELETE";
 config.MAX_SHARED_STORIES = 100;
 config.MAX_COMMENTS = 30;
 config.MAX_Q_RETRIES = 3;
